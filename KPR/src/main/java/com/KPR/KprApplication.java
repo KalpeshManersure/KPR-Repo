d302e86ba73f7273cdef05386513c4852e9d1823
Kalpeshmanersure <Kalpeshmanersure@192.168.1.103>
2022-03-12 14:34:46 +0530

    First commit

---
+++ KPR/src/main/java/com/KPR/KprApplication.java
@@ -0,0 +1,13 @@
+package com.KPR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KprApplication.class, args);
+	}
+
+}
---
+++ KPR/src/test/java/com/KPR/KprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KPR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
